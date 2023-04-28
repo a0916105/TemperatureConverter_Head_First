ea9add7ef2c373dc922c26c5444503b60285250a
a0916105 <whdofjew@gmail.com>
2023-04-28 11:26:58 +0800

    使用composable函式

--- app/src/main/java/tw/idv/jew/temperatureconverter/MainActivity.kt
+++ app/src/main/java/tw/idv/jew/temperatureconverter/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 
 class MainActivity : ComponentActivity() {  //使用Compose需改繼承ComponentActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello friend! Welcome to Jetpack Compose")
+            //使用composable函式
+            Hello("everyone")
         }
     }
+}
+
+//這個註解會將它轉換成composable函式
+@Composable
+fun Hello(name: String) {
+    Text("Hello $name! Welcome to Jetpack Compose")
 }
