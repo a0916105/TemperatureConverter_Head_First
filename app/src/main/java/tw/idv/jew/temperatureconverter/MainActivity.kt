5e4b8aa82f2641fcc927ffac3e334afffcc21786
a0916105 <whdofjew@gmail.com>
2023-04-28 11:34:55 +0800

    使用Column排列方式

--- app/src/main/java/tw/idv/jew/temperatureconverter/MainActivity.kt
+++ app/src/main/java/tw/idv/jew/temperatureconverter/MainActivity.kt
@@ -3,6 +3,7 @@ package tw.idv.jew.temperatureconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
@@ -10,8 +11,11 @@ class MainActivity : ComponentActivity() {  //使用Compose需改繼承Component
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //使用composable函式
-            Hello("everyone")
+            Column {    //使用Column排列方式
+                //使用composable函式
+                Hello("friend")
+                Hello("everyone")
+            }
         }
     }
 }
