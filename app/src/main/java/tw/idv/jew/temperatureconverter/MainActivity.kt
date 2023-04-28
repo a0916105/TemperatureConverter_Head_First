fbbf80f1f6317e54995edfb02eb9990c42eb5b5c
a0916105 <whdofjew@gmail.com>
2023-04-28 13:44:17 +0800

    使用Image composable

--- app/src/main/java/tw/idv/jew/temperatureconverter/MainActivity.kt
+++ app/src/main/java/tw/idv/jew/temperatureconverter/MainActivity.kt
@@ -3,37 +3,41 @@ package tw.idv.jew.temperatureconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {  //使用Compose需改繼承ComponentActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column {    //使用Column排列方式
-                //使用composable函式
-                Hello("friend")
-                Hello("everyone")
-            }
+            MainActivityContent()
         }
     }
 }
 
 //這個註解會將它轉換成composable函式
 @Composable
-fun Hello(name: String) {
-    Text("Hello $name! Welcome to Jetpack Compose")
+fun MainActivityContent() {
+    Image(  //使用Image composable
+        painter = painterResource(R.drawable.sunrise),  //指定圖像
+        contentDescription = "sunrise image",    //圖像的敘述
+        modifier = Modifier //可用來加入額外類別
+            .height(180.dp)
+            .fillMaxWidth(),
+        contentScale = ContentScale.Crop    //自動縮放圖像
+    )
 }
 
 //預覽（只有沒有引數的composable函式）：只有加入此註解的片段程式能使用Split或Design預覽
 @Preview(showBackground = true) //預設背景是透明的，所以使用showBackground = true覆寫設定
 @Composable
 fun PreviewMainActivity() { //要及時預覽的部分（Preview+類別名稱）程式碼因該要相同
-    Column {    //使用Column排列方式
-        //使用composable函式
-        Hello("friend")
-        Hello("everyone")
-    }
+    MainActivityContent()
 }
