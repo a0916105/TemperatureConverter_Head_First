9ebc0884e5768b29951dcaec49ecf54f83d5323c
a0916105 <whdofjew@gmail.com>
2023-04-28 11:16:30 +0800

    使用Text composable來顯示文字

--- app/src/main/java/tw/idv/jew/temperatureconverter/MainActivity.kt
+++ app/src/main/java/tw/idv/jew/temperatureconverter/MainActivity.kt
@@ -3,12 +3,13 @@ package tw.idv.jew.temperatureconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {  //使用Compose需改繼承ComponentActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Text("Hello friend! Welcome to Jetpack Compose")
         }
     }
 }
